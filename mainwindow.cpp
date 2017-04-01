2238ebf215a27f5709b3b531dbf647a9a298772f
Paulo Carvalho <Paulo Carvalho>
2017-04-01 16:47:06 -0300

    DEFFECT: Some icons appearing as low-res in high-res displays.

--- mainwindow.cpp
+++ mainwindow.cpp
@@ -262,27 +262,52 @@ void MainWindow::restoreProjectTreeUIState2()
 
 void MainWindow::refreshTreeStyle()
 {
-    ui->treeProject->setStyleSheet("QTreeView::branch:has-siblings:!adjoins-item { \
-                                   border-image: url(:icons/vline) 0; } \
-            QTreeView::branch:has-siblings:adjoins-item { \
-                 border-image: url(:icons/bmore) 0; \
-             } \
-            \
-             QTreeView::branch:!has-children:!has-siblings:adjoins-item { \
-                 border-image: url(:icons/bend) 0; \
-             } \
-            \
-             QTreeView::branch:has-children:!has-siblings:closed, \
-             QTreeView::branch:closed:has-children:has-siblings { \
-                     border-image: none; \
-                     image: url(:icons/bclosed); \
-             } \
-            \
-             QTreeView::branch:open:has-children:!has-siblings, \
-             QTreeView::branch:open:has-children:has-siblings  { \
-                     border-image: none; \
-                     image: url(:icons/bopen); \
-             }");
+
+    if(Util::getDisplayResolutionClass() == DisplayResolution::NORMAL_DPI)
+        ui->treeProject->setStyleSheet("QTreeView::branch:has-siblings:!adjoins-item { \
+                                       border-image: url(:icons/vline) 0; } \
+                QTreeView::branch:has-siblings:adjoins-item { \
+                     border-image: url(:icons/bmore) 0; \
+                 } \
+                \
+                 QTreeView::branch:!has-children:!has-siblings:adjoins-item { \
+                     border-image: url(:icons/bend) 0; \
+                 } \
+                \
+                 QTreeView::branch:has-children:!has-siblings:closed, \
+                 QTreeView::branch:closed:has-children:has-siblings { \
+                         border-image: none; \
+                         image: url(:icons/bclosed); \
+                 } \
+                \
+                 QTreeView::branch:open:has-children:!has-siblings, \
+                 QTreeView::branch:open:has-children:has-siblings  { \
+                         border-image: none; \
+                         image: url(:icons/bopen); \
+                 }");
+    else
+         ui->treeProject->setStyleSheet("QTreeView::branch:has-siblings:!adjoins-item { \
+                                        border-image: url(:icons32/vline32) 0; } \
+                 QTreeView::branch:has-siblings:adjoins-item { \
+                      border-image: url(:icons32/bmore32) 0; \
+                  } \
+                 \
+                  QTreeView::branch:!has-children:!has-siblings:adjoins-item { \
+                      border-image: url(:icons32/bend32) 0; \
+                  } \
+                 \
+                  QTreeView::branch:has-children:!has-siblings:closed, \
+                  QTreeView::branch:closed:has-children:has-siblings { \
+                          border-image: none; \
+                          image: url(:icons32/bclosed32); \
+                  } \
+                 \
+                  QTreeView::branch:open:has-children:!has-siblings, \
+                  QTreeView::branch:open:has-children:has-siblings  { \
+                          border-image: none; \
+                          image: url(:icons32/bopen32); \
+                  }");
+
 }
 
 
