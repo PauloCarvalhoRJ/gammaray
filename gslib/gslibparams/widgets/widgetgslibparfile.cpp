af8098480e01a195079b3063dd9a7140f7d311ce
Paulo Carvalho <Paulo Carvalho>
2017-10-29 15:41:12 -0200

    BUG FIX: Issue#64 File paths with hyphens are no more allowed (conflict with GSLib parameter syntax).

--- gslib/gslibparams/widgets/widgetgslibparfile.cpp
+++ gslib/gslibparams/widgets/widgetgslibparfile.cpp
@@ -27,13 +27,26 @@ void WidgetGSLibParFile::fillFields(GSLibParFile *param)
 
 void WidgetGSLibParFile::updateValue(GSLibParFile *param)
 {
-    param->_path = ui->txtPath->text();
+    if( ui->txtPath->text().indexOf('-') >=0  ){
+        Application::instance()->logError("WidgetGSLibParFile::updateValue(): File paths containing hyphens conflict with GSLib parameter file syntax.");
+        Application::instance()->logError("     Parameter " + param->getName() + " value reset to empty.");
+        param->_path = "";
+        ui->txtPath->setText("");
+    } else {
+        param->_path = ui->txtPath->text();
+    }
 }
 
 void WidgetGSLibParFile::onBtnFileClicked(bool)
 {
     QString file = QFileDialog::getOpenFileName(this, "select file", Application::instance()->getProject()->getPath());
     if( ! file.isEmpty() ){
-        ui->txtPath->setText( file );
+        if( file.indexOf('-') >=0  ){
+            Application::instance()->logError("WidgetGSLibParFile::onBtnFileClicked(): File paths containing hyphens conflict with GSLib parameter file syntax.");
+            Application::instance()->logError("     Field value reset to empty.");
+            ui->txtPath->setText("");
+        } else {
+            ui->txtPath->setText( file );
+        }
     }
 }
