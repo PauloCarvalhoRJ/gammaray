7b60a4c554a6514ea14822b1b5f4684f73829e80
Paulo Carvalho <Paulo Carvalho>
2017-12-10 14:25:51 -0200

    Added new author to program manual and the Help/About dialog.

--- dialogs/aboutdialog.cpp
+++ dialogs/aboutdialog.cpp
@@ -14,7 +14,7 @@ GSLib geostatistics software.\n\nGSLib by Centre for Computational Geostatistics
 University of Alberta.\n\n").\
          append(APP_NAME).append(" uses GhostScript to parse GSLib PostScript plots. \
 GhostScript by Artifex Software Inc.\n\n").\
-         append(APP_NAME).append(" by Paulo R. M. Carvalho (paulo.r.m.carvalho@gmail.com) and the code contributors (see LICENSE.md file)\n\n").\
+         append(APP_NAME).append(" by Paulo R. M. Carvalho (paulo.r.m.carvalho@gmail.com), Pericles Machado (periclesraskolnikoff@gmail.com) and the code contributors (see LICENSE.md file)\n\n").\
          append(APP_NAME).append(" is licensed under the Creative Commons BY-SA license v3.0."));
 
     if( Util::getDisplayResolutionClass() == DisplayResolution::HIGH_DPI ){
