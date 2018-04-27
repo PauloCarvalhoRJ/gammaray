3d1bcb9571f4e8973df1a655821dce26e2df1ab9
Paulo Carvalho <ur5m@petrobras.com.br>
2018-04-27 17:21:34 -0300

    WIP: variographic decomposition workflow.

--- imagejockey/vardecomp/variographicdecompositiondialog.cpp
+++ imagejockey/vardecomp/variographicdecompositiondialog.cpp
@@ -4,8 +4,10 @@
 #include "../widgets/ijvariableselector.h"
 #include "../ijabstractcartesiangrid.h"
 #include "../ijabstractvariable.h"
+#include "../imagejockeyutils.h"
 #include "spectral/svd.h"
 
+#include <QMessageBox>
 #include <QProgressDialog>
 
 VariographicDecompositionDialog::VariographicDecompositionDialog(const std::vector<IJAbstractCartesianGrid *> &&grids, QWidget *parent) :
@@ -42,6 +44,11 @@ void VariographicDecompositionDialog::doVariographicDecomposition()
 	IJAbstractCartesianGrid* grid = m_gridSelector->getSelectedGrid();
 	IJAbstractVariable* variable = m_variableSelector->getSelectedVariable();
 
+	if( ! grid && ! variable ){
+		QMessageBox::critical( this, "Error", "Please, select a grid and a variable.");
+		return;
+	}
+
 	// Get the grid's dimensions.
 	unsigned int nI = grid->getNI();
 	unsigned int nJ = grid->getNJ();
@@ -50,8 +57,15 @@ void VariographicDecompositionDialog::doVariographicDecomposition()
 	// Fetch data from the data source.
 	grid->dataWillBeRequested();
 
-	// PRODUCTS: grid with phase of FFT transform of the input variable.
+	// The user-given number of geological factors (m).
+	int m = ui->spinNumberOfGeologicalFactors->value();
+
+	// PRODUCTS: 1) grid with phase of FFT transform of the input variable.
+	//           2) collection of grids with the fundamental SVD factors of the variable's varmap.
+	//           3) n: number of fundamental factors.
 	spectral::complex_array gridMagnitudeAndPhaseParts( nI, nJ, nK );
+	std::vector< spectral::array > svdFactors;
+	int n = 0;
 	{
 		// Perform FFT to get a grid of complex numbersi in rectangular form: real part (a), imaginary part (b).
 		// PRODUCTS: 2 grids: real and imaginary parts.
@@ -107,7 +121,139 @@ void VariographicDecompositionDialog::doVariographicDecomposition()
 			QCoreApplication::processEvents(); //let Qt repaint widgets
 			spectral::backward( gridVarmap, gridNormSquaredAndZeroPhase );
 		}
+
+		//Compute SVD of varmap
+		{
+			//Get the number of usable fundamental SVD factors.
+			{
+				QProgressDialog progressDialog;
+				progressDialog.setRange(0,0);
+				progressDialog.setLabelText("Computing SVD factors of varmap...");
+				progressDialog.show();
+				QCoreApplication::processEvents();
+				spectral::SVD svd = spectral::svd( gridVarmap );
+				//get the list with the factor weights (information quantity)
+				spectral::array weights = svd.factor_weights();
+				progressDialog.hide();
+				//get the number of fundamental factors that have 100% of information content.
+				{
+					double cumulative = 0.0;
+					uint i = 0;
+					for(; i < weights.size(); ++i){
+						cumulative += weights.d_[i];
+						if( cumulative > 0.99999 )
+							break;
+					}
+					n = i+1;
+				}
+				if( n < 3 ){
+					QMessageBox::warning( this, "Warning", "The data's varmap must be decomposable in at least three usable fundamental factors to proceed.");
+					return;
+				}
+				//Get the usable fundamental SVD factors.
+				{
+					QProgressDialog progressDialog;
+					progressDialog.setRange(0,0);
+					progressDialog.show();
+					for (long i = 0; i < n; ++i) {
+						progressDialog.setLabelText("Retrieving SVD factor " + QString::number(i+1) + " of " + QString::number(n) + "...");
+						QCoreApplication::processEvents();
+						spectral::array factor = svd.factor(i);
+						svdFactors.push_back( std::move( factor ) );
+					}
+				}
+			}
+		}
 	}
 
+	//Make the A matrix of the linear system originated by the information
+	//conservation constraint (see program manual for the complete theory).
+	//elements are initialized to zero.
+	spectral::array A( (spectral::index)n, (spectral::index)m*n );
+	for( int line = 0; line < n; ++line )
+		for( int column = line * m; column < ((line+1) * m); ++column)
+			 A( line, column ) = 1.0;
+
+	//Make the B matrix of said system
+	spectral::array B( (spectral::index)n, (spectral::index)1 );
+	for( int line = 0; line < n; ++line )
+		B( line, 0 ) = 1.0;
+
+	//Make the identity matrix to find the sundamental factors weights a from the parameters w: [a] = Adagger.B + (I-Adagger.A)[w]
+	spectral::array I( (spectral::index)m*n, (spectral::index)m*n );
+	for( int i = 0; i < m*n; ++i )
+		I( i, i ) = 1.0;
+
+	//Get the U, Sigma and V* matrices from SVD on A.
+	spectral::SVD svd = spectral::svd( A );
+	spectral::array U = svd.U();
+	spectral::array Sigma = svd.S();
+	spectral::array V = svd.V(); //SVD yields V* already transposed, thus V, but to check, you must transpose V to get A = U.Sigma.V*
+
+	//Make a full Sigma matrix (to be compatible with multiplication with the othe matrices)
+	{
+		spectral::array SigmaTmp( (spectral::index)n, (spectral::index)m*n );
+		for( int i = 0; i < n; ++i)
+			SigmaTmp(i, i) = Sigma.d_[i];
+		Sigma = SigmaTmp;
+	}
+
+	//Make U*
+	//U is square, symmetrical and contains only real number, thus U's transpose conjugate is itself.
+	spectral::array Ustar( U );
+
+	//Make Sigmadagger (pseudoinverse of Sigma)
+	spectral::array SigmaDagger( Sigma );
+	{
+		//compute reciprocals of the non-zero elements in the main diagonal.
+		for( int i = 0; i < n; ++i){
+			double value = SigmaDagger(i, i);
+			if( ! ImageJockeyUtils::almostEqual2sComplement( value, 0.0, 1 ) )
+				SigmaDagger( i, i ) = 1.0 / value;
+		}
+		//transpose
+		Eigen::MatrixXd tmp = spectral::to_2d( SigmaDagger );
+		tmp.transposeInPlace();
+		SigmaDagger = spectral::to_array( tmp );
+	}
+
+	//Make Adagger (pseudoinverse of A) by "reversing" the transform U.Sigma.V*,
+	//hence, Adagger = V.Sigmadagger.Ustar .
+	spectral::array Adagger;
+	{
+		Eigen::MatrixXd eigenV = spectral::to_2d( V );
+		Eigen::MatrixXd eigenSigmadagger = spectral::to_2d( SigmaDagger );
+		Eigen::MatrixXd eigenUstar = spectral::to_2d( Ustar );
+		Eigen::MatrixXd eigenAdagger = eigenV * eigenSigmadagger * eigenUstar;
+		Adagger = spectral::to_array( eigenAdagger );
+	}
+
+	//Initialize the vector of system parameters [w]=[0]
+	spectral::array vw( (spectral::index)m*n );
+
+	//-------------------------OPTIMIZATION LOOP BEGINS HERE-------------------------------------------
+
+	//Compute the vector of weights [a] = Adagger.B + (I-Adagger.A)[w]
+	spectral::array va;
+	{
+		Eigen::MatrixXd eigenAdagger = spectral::to_2d( Adagger );
+		Eigen::MatrixXd eigenB = spectral::to_2d( B );
+		Eigen::MatrixXd eigenI = spectral::to_2d( I );
+		Eigen::MatrixXd eigenA = spectral::to_2d( A );
+		Eigen::MatrixXd eigenvw = spectral::to_2d( vw );
+		Eigen::MatrixXd eigenva = eigenAdagger * eigenB + ( eigenI - eigenAdagger * eigenA ) * eigenvw;
+		va = spectral::to_array( eigenva );
+	}
+
+	//Make the m geological factors (expected variographic structures)
+	std::vector< spectral::array > geologicalFactors;
+	for( int iGeoFactor = 0; iGeoFactor < m; ++iGeoFactor){
+		spectral::array geologicalFactor( (spectral::index)nI, (spectral::index)nK, (spectral::index)nJ );
+		for( int iSVDFactor = 0; iSVDFactor < n; ++iSVDFactor){
+			//TODO: implement * operator ( spectral::array with double )
+			geologicalFactor = geologicalFactor + svdFactors[iSVDFactor] * va.d_[ iGeoFactor * m + iSVDFactor ];
+		}
+		geologicalFactors.push_back( std::move( geologicalFactor ) );
+	}
 
 }
