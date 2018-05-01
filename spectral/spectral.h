80b66b8574c5afab25702cdc2f83e1124a3ca81a
Paulo Carvalho <Paulo Carvalho>
2018-05-01 19:10:46 -0300

    The * operator between two spectral::array is now const.

--- spectral/spectral.h
+++ spectral/spectral.h
@@ -100,7 +100,7 @@ struct array {
 
     array &operator+=(const array &other);
 
-    array operator*( double scalar );
+    array operator*( double scalar ) const;
 
     array operator-( const array &other );
 
@@ -242,6 +242,12 @@ void print( const array &A );
  *  separation in the center).
  */
 array shiftByHalf(const array &in);
+
+/**
+ * Computes element-wise the sum of the absolute value of the differences between
+ * the two given data arrays.  Both arrays must have the same number of elements.
+ */
+double sumOfAbsDifference( const array &one, const array &other );
 
 } // namepsace spectral
 
