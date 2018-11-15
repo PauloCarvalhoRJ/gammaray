372e4b4eba4aba27f63d41428fa41384c2aa6184
Paulo Carvalho <Paulo Carvalho>
2018-11-15 11:00:27 -0200

    EMD: WIP.

--- imagejockey/emd/emdanalysisdialog.cpp
+++ imagejockey/emd/emdanalysisdialog.cpp
@@ -40,24 +40,50 @@ void EMDAnalysisDialog::onPerformEMD()
     //create the local minima envelope data set, initialized with no-data-values.
     spectral::array localMinimaEnvelope( nI, nJ, nK, NDV );
 
-    //size of half-window (1 means a 3x3x3 window)
+    //size of half-window (1 means a 3x3x3 window with the target cell in the center)
     int halfWindowSize = 1;
 
     //for each cell...
     for( int k = 0; k < nK; ++k )
         for( int j = 0; j < nJ; ++j )
             for( int i = 0; i < nI; ++i ){
+                bool is_a_local_minimum = true;
+                bool is_a_local_maximum = true;
                 //...get its value
                 double cellValue = m_inputGrid->getData( m_inputVariableIndex, i, j, k );
-                //...get the neighboring values
+                //...evaluate the neighboring values
                 for( int offsetK = -halfWindowSize; offsetK <= halfWindowSize; ++offsetK )
                     for( int offsetJ = -halfWindowSize; offsetJ <= halfWindowSize; ++offsetJ )
                         for( int offsetI = -halfWindowSize; offsetI <= halfWindowSize; ++offsetI ){
                             int neighI = nI + offsetI;
                             int neighJ = nJ + offsetJ;
                             int neighK = nK + offsetK;
-                        }
-            }
+                            //if's to handle border cases
+                            if( neighI >= 0 && neighI < nI &&
+                                neighJ >= 0 && neighJ < nJ &&
+                                neighK >= 0 && neighK < nK ){
+                                //get neighboring value
+                                double neighValue = m_inputGrid( m_inputVariableIndex,
+                                                                 neighI,
+                                                                 neighJ,
+                                                                 neighK );
+                                //if it is a valid value
+                                if( ! m_inputGrid->isNoDataValue( neighValue )){
+                                    //if the cell value is less than a neighbor's...
+                                    if( cellValue < neighValue )
+                                        //...it can't be a local maximum
+                                        is_a_local_maximum = false;
+                                    //if the cell value is greater than a neighbor's...
+                                    if( cellValue > neighValue )
+                                        //...it cant be a local minimum
+                                        is_a_local_minimum = false;
+                                }
+                            }
+                        } // --- evaluate the neighboring cells
+                //if the cell is a local maximum...
+                if( is_a_local_maximum )
+                    STOPPED_HERE;
+            } // --- for each cell
 
 
 }
