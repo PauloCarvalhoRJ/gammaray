3d9bc67629f6685f386fc2de8665ab2d1a1a2ffa
Paulo Carvalho <paulo.r.m.carvalho@gmail.com>
2019-01-16 10:15:53 -0200

    Segment Sets: adding functionality to SegmentSetDialog.

--- dialogs/segmentsetdialog.cpp
+++ dialogs/segmentsetdialog.cpp
@@ -1,11 +1,43 @@
 #include "segmentsetdialog.h"
 #include "ui_segmentsetdialog.h"
+#include "exceptions/invalidgslibdatafileexception.h"
+#include "util.h"
+#include <QMessageBox>
+
 
 SegmentSetDialog::SegmentSetDialog(QWidget *parent, const QString file_path) :
     QDialog(parent),
     ui(new Ui::SegmentSetDialog)
 {
     ui->setupUi(this);
+
+    setWindowTitle( "Segment set metadata" );
+
+    this->_file_path = file_path;
+
+    //read file content sample
+    Util::readFileSample( this->ui->txtFileSample, file_path );
+
+    //get data file variable names to populate the combo boxes.
+    try{
+        QStringList field_list = Util::getFieldNames( file_path );
+        ui->cmbX0->addItem(" ");
+        ui->cmbX0->addItems( field_list );
+        ui->cmbY0->addItem(" ");
+        ui->cmbY0->addItems( field_list );
+        ui->cmbZ0->addItem(" ");
+        ui->cmbZ0->addItems( field_list );
+        ui->cmbX1->addItem(" ");
+        ui->cmbX1->addItems( field_list );
+        ui->cmbY1->addItem(" ");
+        ui->cmbY1->addItems( field_list );
+        ui->cmbZ1->addItem(" ");
+        ui->cmbZ1->addItems( field_list );
+    }catch ( InvalidGSLibDataFileException& ex ){
+        QMessageBox::critical( this, "Error", "Invalid GSLib data file." );
+    }
+
+    adjustSize();
 }
 
 SegmentSetDialog::~SegmentSetDialog()
