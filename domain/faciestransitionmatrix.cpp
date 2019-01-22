78f01c53b3deb0f1dc5ad3f9c01ef5b0cffa87f6
Paulo Carvalho <paulo.r.m.carvalho@gmail.com>
2019-01-22 16:28:40 -0200

    Introducing the TransProbMatrix class.

---
+++ domain/faciestransitionmatrix.cpp
@@ -0,0 +1,6 @@
+#include "faciestransitionmatrix.h"
+
+FaciesTransitionMatrix::FaciesTransitionMatrix()
+{
+
+}
---
+++ domain/faciestransitionmatrix.h
@@ -0,0 +1,11 @@
+#ifndef FACIESTRANSITIONMATRIX_H
+#define FACIESTRANSITIONMATRIX_H
+
+
+class FaciesTransitionMatrix : public File
+{
+public:
+    FaciesTransitionMatrix();
+};
+
+#endif // FACIESTRANSITIONMATRIX_H
