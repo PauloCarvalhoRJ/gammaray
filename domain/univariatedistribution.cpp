f7d079fb5a4573726ca74602fefece8a3505562c
Paulo Roberto Moura de Carvalho <paulo_moura@petrobras.com.br>
2020-02-19 07:50:49 -0300

    UnivariateDistribution: new method: getValueFromCumulativeFrequency().

--- domain/univariatedistribution.cpp
+++ domain/univariatedistribution.cpp
@@ -1,8 +1,11 @@
 #include "univariatedistribution.h"
 #include "util.h"
 
+#include "domain/application.h"
 
-UnivariateDistribution::UnivariateDistribution(const QString path) : Distribution( path )
+UnivariateDistribution::UnivariateDistribution(const QString path) :
+    Distribution( path ),
+    m_data( path )
 {
 }
 
@@ -14,4 +17,56 @@ QIcon UnivariateDistribution::getIcon()
         return QIcon(":icons32/unidist32");
 }
 
+double UnivariateDistribution::getValueFromCumulativeFrequency(double cumulativeProbability) const
+{
+    //result is NaN by default
+    double result = std::numeric_limits<double>::quiet_NaN();
 
+    //get the columns for the Z and P values of the distribution
+    int zValueIndex = getTheColumnWithValueRole()-1;
+    int pValueIndex = getTheColumnWithProbabilityRole()-1;
+
+    //sanity checks
+    if( m_data.getDataTable().empty() ){
+        Application::instance()->logError("UnivariateDistribution::getValueFromCumulativeFrequency(): distribution data not loaded. "
+                                          "Make sure there is a prior call to UnivariateDistribution::readFromFS().");
+        return result;
+    }
+    if( zValueIndex < 0 ){
+        Application::instance()->logError("UnivariateDistribution::getValueFromCumulativeFrequency(): no field or more than one field with Z-Value role.");
+        return result;
+    }
+    if( pValueIndex < 0 ){
+        Application::instance()->logError("UnivariateDistribution::getValueFromCumulativeFrequency(): no field or more than one field with P-Value role.");
+        return result;
+    }
+
+    //traverse the distribution values table to determine the Z-value corresponding
+    //to the given cumulative probability
+    double previousZValue = 0.0;
+    double previousPValue = 0.0;
+    double previousCumulativeP = 0.0;
+    double cumulativeP = 0.0;
+    for( int i = 0; i < m_data.getDataTable().size(); ++i ){
+        const std::vector<double>& record = m_data.getDataTable()[i];
+        double zValue = record[ zValueIndex ];
+        double pValue = record[ pValueIndex ];
+        cumulativeP += pValue;
+        if( i > 0 ){
+            if( cumulativeProbability < cumulativeP ){
+                //the resulting z value is a linear interpolation between two cumulative probabilities and two z-values.
+                return Util::linearInterpolation( cumulativeProbability, previousCumulativeP, cumulativeP, previousZValue, zValue );
+            }
+        }
+        previousZValue = zValue;
+        previousPValue = pValue;
+        previousCumulativeP = cumulativeP;
+    }
+
+    return result;
+}
+
+void UnivariateDistribution::readFromFS()
+{
+    m_data.loadData();
+}
--- domain/univariatedistribution.h
+++ domain/univariatedistribution.h
@@ -2,6 +2,7 @@
 #define UNIVARIATEDISTRIBUTION_H
 
 #include "distribution.h"
+#include "domain/pointset.h"
 
 /**
  * @brief The UnivariateDistribution class represents a univariate distribution model, normally
@@ -14,15 +15,29 @@ class UnivariateDistribution : public Distribution
 public:
     UnivariateDistribution( const QString path );
 
+    /**
+     * Returns a value given a cumulative frequency.
+     * The passed cumulative frequency is normally drawn from a random number generator.
+     * This method is typically called from value drawing code in simulation algorithms.
+     * Returns NaN if somehow the method fails to find the category code corresponding to passed
+     * cumulative frequency.
+     */
+    double getValueFromCumulativeFrequency( double cumulativeProbability ) const;
+
 // File interface
 public:
     QString getFileType(){ return "UNIDIST"; }
     bool isDataFile(){ return false; }
 	bool isDistribution(){ return true; }
+    virtual void readFromFS();
 
 // ProjectComponent interface
 public:
     QIcon getIcon();
+
+protected:
+    /** PointSet object used as convenient way to manage the distribution's data. */
+    PointSet m_data;
 };
 
 #endif // UNIVARIATEDISTRIBUTION_H
