c6563d4233264559c3eef6f6fff41d98e6d03b6b
Paulo Roberto Moura de Carvalho <paulo_moura@petrobras.com.br>
2020-06-15 20:03:22 -0300

    LineChartWidget: WIP.

--- widgets/linechartwidget.cpp
+++ widgets/linechartwidget.cpp
@@ -53,25 +53,49 @@ void LineChartWidget::setData(const std::vector<std::vector<double> > &data, int
     double minX = std::numeric_limits<double>::max();
     double maxX = -std::numeric_limits<double>::max();
 
-    // Get the number of dependent variables.
-    int nVarsY = data[0].size() - 1;
+    // Clears all current data series.
+    m_chart->removeAllSeries();
 
-    // Create a number of Y data series and axes.
+    // Create a number of data series and axes. One series per dependent variable.
+    for( int iSeries = 0; iSeries < data[0].size(); ++iSeries ){
+        if( iSeries != indexForXAxis ){
 
-    // For each sample...
-    for( const std::vector< double > sample : data ){
-        // Get the independent variable.
-        double valueX = sample[ indexForXAxis ];
-        // Update X value limits.
-        minX = std::min( minX, valueX );
-        maxX = std::max( maxX, valueX );
-        // Get the dependent variable(s).
-        for( int iValue = 0; iValue < sample.size(); ++iValue ){
-            if( iValue != indexForXAxis ){
+            // Define limits for the Y axis (one per dependent variable).
+            double minY = std::numeric_limits<double>::max();
+            double maxY = -std::numeric_limits<double>::max();
 
-            }
-        }
-    }
+            // Create a Y-axis
+            QtCharts::QValueAxis *axisY = new QtCharts::QValueAxis();
+
+            // Create a series
+            QtCharts::QLineSeries* series = new QtCharts::QLineSeries();
+
+            // For each sample...
+            for( const std::vector< double > sample : data ){
+                // Get the independent variable.
+                double valueX = sample[ indexForXAxis ];
+                // Update X value limits.
+                minX = std::min( minX, valueX );
+                maxX = std::max( maxX, valueX );
+                // Get the dependent variable.
+                double valueY = sample[ iSeries ];
+                // Update Y value limits for the series.
+                minY = std::min( minY, valueY );
+                maxY = std::max( maxY, valueY );
+                // Apend the XY value pair to the series.
+                series->append( valueX, valueY );
+            } //for each sample
+
+            //Update the Y axis (one per dependent variable).
+            axisY->setRange( minY, maxY );
+
+            m_chart->addSeries( series );
+            m_chart->addAxis(axisY, Qt::AlignLeft);
+            series->attachAxis( m_axisX );
+            series->attachAxis( axisY );
+
+        } //if to skip the independent variable, which is assigned to X-axis
+    } //
 
     //Update the X axis.
     m_axisX->setRange( minX, maxX );
