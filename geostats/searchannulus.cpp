d39e263648c7c2f8316e4af003a0832c20a13b8f
Paulo Roberto Moura de Carvalho <paulo_moura@petrobras.com.br>
2024-04-03 19:26:38 -0300

    Introducing a new search strategy: SearchAnnulus.

---
+++ geostats/searchannulus.cpp
@@ -0,0 +1,36 @@
+#include "searchannulus.h"
+
+#include <limits>
+
+SearchAnnulus::SearchAnnulus(double innerRadius, double outerRadius,
+                             uint minNumberOfSamples, uint maxNumberOfSamples) :
+    m_innerRadius(innerRadius),
+    m_outerRadius(outerRadius),
+    m_maxNumberOfSamples(maxNumberOfSamples),
+    m_minNumberOfSamples(minNumberOfSamples)
+{}
+
+void SearchAnnulus::getBBox(double centerX, double centerY, double centerZ,
+                            double &minX, double &minY, double &minZ,
+                            double &maxX, double &maxY, double &maxZ) const
+{
+    maxX = centerX + m_outerRadius;
+    minX = centerX - m_outerRadius;
+    maxY = centerY + m_outerRadius;
+    minY = centerY - m_innerRadius;
+    maxZ = centerZ + std::numeric_limits<double>::max();
+    minZ = centerZ - std::numeric_limits<double>::max();
+}
+
+bool SearchAnnulus::isInside(double centerX, double centerY, double centerZ,
+                             double x, double y, double z) const
+{
+    double localX = x - centerX;
+    double localY = y - centerY;
+    double outer_dx = localX/m_outerRadius;
+    double outer_dy = localY/m_outerRadius;
+    double inner_dx = localX/m_innerRadius;
+    double inner_dy = localY/m_innerRadius;
+    return ( outer_dx*outer_dx + outer_dy*outer_dy <= 1.0 ) &&
+           ( inner_dx*inner_dx + inner_dy*inner_dy  > 1.0 );
+}
