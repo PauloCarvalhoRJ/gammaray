8d606edbf90c83a57960a37a2c7c1cf49de71562
Paulo Roberto Moura de Carvalho <paulo_moura@petrobras.com.br>
2024-04-08 18:23:21 -0300

    Geostatistics: Introducing a new search neighborhood class: SearchWasher.

---
+++ geostats/searchwasher.cpp
@@ -0,0 +1,35 @@
+#include "searchwasher.h"
+
+SearchWasher::SearchWasher(double innerRadius, double outerRadius, double zThickness,
+                           uint minNumberOfSamples, uint maxNumberOfSamples) :
+    SearchAnnulus( innerRadius, outerRadius, minNumberOfSamples, maxNumberOfSamples ),
+    m_zThickness( zThickness )
+{}
+
+void SearchWasher::getBBox(double centerX, double centerY, double centerZ,
+                            double &minX, double &minY, double &minZ,
+                            double &maxX, double &maxY, double &maxZ) const
+{
+    maxX = centerX + m_outerRadius;
+    minX = centerX - m_outerRadius;
+    maxY = centerY + m_outerRadius;
+    minY = centerY - m_outerRadius;
+    maxZ = centerZ + m_zThickness/2;
+    minZ = centerZ - m_zThickness/2;
+}
+
+bool SearchWasher::isInside(double centerX, double centerY, double centerZ,
+                             double x, double y, double z) const
+{
+    double localX = x - centerX;
+    double localY = y - centerY;
+    double localZ = z - centerZ;
+    double outer_dx = localX/m_outerRadius;
+    double outer_dy = localY/m_outerRadius;
+    double inner_dx = localX/m_innerRadius;
+    double inner_dy = localY/m_innerRadius;
+    return ( outer_dx*outer_dx + outer_dy*outer_dy <= 1.0 ) &&
+           ( inner_dx*inner_dx + inner_dy*inner_dy  > 1.0 ) &&
+           ( localZ >= -m_zThickness/2 && localZ <= m_zThickness/2 );
+}
+
---
+++ geostats/searchwasher.h
@@ -0,0 +1,38 @@
+#ifndef SEARCHWASHER_H
+#define SEARCHWASHER_H
+
+#include "geostats/searchannulus.h"
+
+/**
+ * The SearchWasher class represents a search neighborhood with the shape of a washer.
+ * The shape extends that of the SearchAnnulus to include a [normally small] thickness in Z direction
+ * so it can be used for lateral searches in 3D datasets like that of the SearchAnnulus in 2D datasets.
+ */
+class SearchWasher : public SearchAnnulus
+{
+public:
+    SearchWasher(double innerRadius, double outerRadius, double zThickness,
+                 uint minNumberOfSamples, uint maxNumberOfSamples);
+
+    /** Move constructor. */
+    SearchWasher( SearchWasher&& right_hand_side );
+
+    ////-------------SearchNeighborhood interface------------------------------------------
+    virtual void getBBox( double centerX, double centerY, double centerZ,
+                          double& minX, double& minY, double& minZ,
+                          double& maxX, double& maxY, double& maxZ ) const;
+
+    virtual bool isInside(double centerX, double centerY, double centerZ,
+                          double x, double y, double z ) const;
+
+    virtual bool hasSpatialFiltering() const { return false; }
+
+    virtual void performSpatialFilter( double centerX, double centerY, double centerZ,
+                                       std::vector< IndexedSpatialLocationPtr >& samplesLocations,
+                                       const SearchStrategy& parentSearchStrategy ) const {}
+    ////-------------------------------------------------------------------------------------
+
+    double m_zThickness;
+};
+
+#endif // SEARCHWASHER_H
