5bbdcf0d399ae5d246b0c4ccc286c2af534c0adc
Paulo Roberto Moura de Carvalho <paulo_moura@petrobras.com.br>
2024-04-17 19:36:00 -0300

    BoundingBox: new constructor initializing all members and new methods to compute the box's center XYZ coordinates.

--- geometry/boundingbox.h
+++ geometry/boundingbox.h
@@ -10,9 +10,16 @@ class BoundingBox
 public:
     BoundingBox();
 
+    BoundingBox(double minX, double minY, double minZ,
+                double maxX, double maxY, double maxZ);
+
     double getXsize() const { return m_maxX - m_minX; }
     double getYsize() const { return m_maxY - m_minY; }
     double getZsize() const { return m_maxZ - m_minZ; }
+
+    double getCenterX() const { return (m_maxX+m_minX)/2; }
+    double getCenterY() const { return (m_maxY+m_minY)/2; }
+    double getCenterZ() const { return (m_maxZ+m_minZ)/2; }
 
     double m_minX, m_minY, m_minZ, m_maxX, m_maxY, m_maxZ;
 };
