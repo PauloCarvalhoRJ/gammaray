9fddb8f888fc3c28b7dda5df3c1fee6494713637
Paulo Carvalho <paulo.r.m.carvalho@gmail.com>
2024-07-17 18:18:22 -0300

    Build w/ Conan: added the double-conversion dependency (needed when building with Visual Studio, which doesn'th have a libc++.

---
+++ conan2_recipes/double-conversion/3.3.0/conanfile.py
@@ -0,0 +1,112 @@
+from conan import ConanFile
+from conan.tools.cmake import CMake, CMakeToolchain, cmake_layout
+from conan.tools.files import copy, get, rmdir, rm, unzip
+from conan.tools.microsoft import check_min_vs
+import os
+
+required_conan_version = ">=1.54.0"
+
+# This recipe builds, installs and tests libJPEG 9e.
+# The double-conversion dependency is needed by some packages (e.g. Qt) building in Windows/Visual Studio, when libc++ is not available.
+
+# All recipes are classes derived from the abstract class ConanFile that implement its "virtual" methods.
+class DoubleConversionConan(ConanFile):
+    # This is the name that will be used to locate the package/dependency in Conan repository.
+    name = "double-conversion"
+    # Sets the version of this package/dependency and is used by Conan to manage packages accurately.
+    version = "3.3.0"
+    # Declare where the content was obtained from.
+    url = "https://github.com/conan-io/conan-center-index"
+    # Declare the home page of the dependency's project.
+    homepage = "https://github.com/google/double-conversion"
+    # A brief description of what this recipe does.
+    description = "Efficient binary-decimal and decimal-binary conversion routines for IEEE doubles."
+    # Declare the license of the dependency.
+    license = "BSD-3-Clause"
+    # State some keywords describing what the dependency does.
+    topics = ("google", "decimal-binary", "conversion")
+    # Hints Conan of what kind of information to propagate to dependant packages (e.g. include dir, libs dir, etc.).
+    package_type = "library"
+    # Declare what information are needed to build and uniquely identify this package in addition to name and version.
+    settings = "os", "arch", "compiler", "build_type"
+    # A set of additional custom options that are necessary to build the dependency.
+    options = {
+        "shared": [True, False],
+        "fPIC": [True, False],
+    }
+    # Declare which are the default values to the options in the options attribute.
+    default_options = {
+        "shared": False,
+        "fPIC": True,
+    }
+    # A list of files that should be exported (copied) from here (the Git repository) to the source directory in
+    # Conan's workspace.
+    exports_sources = "double-conversion-{}.tar.gz".format(version)
+    # Declares this dependency's dependencies needed for building.
+    # In this case, the Ninja and CMake binaries that must exist in CMake package (named "cmake_installer" in its recipe).
+    # NOTE: this attribute used to be build_requires in Conan 1 and was kept in Conan 2 for backwards compatibility.
+    tool_requires = "ninja/1.12.1", "cmake_installer/3.29.3"
+
+    # Configure or constrain the available options in a package before assigning them a value. A typical use case is
+    # to remove an option in a given platform. For example, the SSE2 flag doesn’t exist in architectures different 
+    # than 32 bits, so it should be removed in this method.
+    def config_options(self):
+        if self.settings.os == "Windows":
+            del self.options.fPIC
+
+    # Runs the configuration of settings and options in the recipe for later use in the different methods like 
+    # generate(), build() or package(). This method executes while building the dependency graph and expanding the
+    # packages dependencies.
+    def configure(self):
+        if self.options.shared:
+            self.options.rm_safe("fPIC")
+
+    # Allows to customize some standard attributes (e.g. self.folders).
+    # In this case, we inform Conan that the source directory is the "src" subdirectory in 
+    # the root source directory in Conan workspace.
+    def layout(self):
+        cmake_layout(self, src_folder="src")
+
+    # This callback is evoked to perform some validations (e.g. unsupported operating system) before building.
+    # The detection of an invalid configuration must be signaled by raising an exception.
+    def validate(self):
+        check_min_vs(self, "190")
+
+    # Performs the actions necessary to obtain the source files.
+    # In this case, as the files are already in the source repository, this only
+    # unzips the source files into the build directory in Conan's workspace.
+    def source(self):
+        # get(self, **self.conan_data["sources"][self.version], strip_root=True) --> in the original recipe, the archive is downloaded upon running this recipe.
+        unzip(self, filename=os.path.join(self.source_folder, "..", "double-conversion-{}.tar.gz".format(self.version)), destination=".")
+        copy(self, pattern="*", src=os.path.join(self.source_folder, "double-conversion-{}".format(self.version)), dst=".")
+
+    # Contains the generation steps for the package.
+    # E.g. runs CMake to generate a runnable Makefile or VS Solution.
+    def generate(self):
+        tc = CMakeToolchain(self)
+        tc.variables["CMAKE_WINDOWS_EXPORT_ALL_SYMBOLS"] = True
+        tc.generate()
+
+    # Contains the package build instructions.
+    # Generates build artifacts in the build directory from the source files in the source directory.
+    def build(self):
+        cmake = CMake(self)
+        cmake.configure()
+        cmake.build()
+
+    # Copies the wanted build artifacts (e.g. shared libraries) from the build directory into the 
+    # package directory so they can be used by other packages that are dependant of this one.
+    # Running cmake.install() is often a shortcut to copy all the necessary files (e.g. headers and libraries).
+    def package(self):
+        copy(self, "LICENSE", src=self.source_folder, dst=os.path.join(self.package_folder, "licenses"))
+        cmake = CMake(self)
+        cmake.install()
+        rmdir(self, os.path.join(self.package_folder, "lib", "cmake"))
+        rm(self, "*.pdb", os.path.join(self.package_folder, "bin"))
+
+    # This is called when running recipes for packages which are dependant of this one.
+    # E.g. In their `requires = ...` or `tool_requires = ...` attributes.
+    def package_info(self):
+        self.cpp_info.set_property("cmake_file_name", "double-conversion")
+        self.cpp_info.set_property("cmake_target_name", "double-conversion::double-conversion")
+        self.cpp_info.libs = ["double-conversion"]
